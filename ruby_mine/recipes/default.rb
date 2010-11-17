2f44f8d6135eac8b4e497707aff437767a06bc18
Gabe Varela <gvarela@gmail.com>
2010-11-17 14:59:29 -0700

    add a rubymine cookbook for installing with the factory preferences

---
+++ ruby_mine/recipes/default.rb
@@ -0,0 +1,17 @@
+application "RubyMine 3.0" do
+  source "http://download.jetbrains.com/ruby/RubyMine-3.0-RC.dmg"
+end
+
+bash "setup factory preferences" do
+  user node[:homebrew][:user]
+  code <<-EOH
+mkdir -p /Users/#{node[:homebrew][:user]}/Library/Preferences/RubyMine30
+cd /Users/#{node[:homebrew][:user]}/Library/Preferences/RubyMine30
+git init
+git remote add origin git://github.com/factorylabs/ruby-mine-settings.git
+git fetch
+git branch master origin/master
+git reset --hard
+  EOH
+  not_if "test -f /Users/#{node[:homebrew][:user]}/Library/Preferences/RubyMine30/colors/LessVibrantInk.xml", :user => node[:homebrew][:user]
+end
