82b6ec59c16d26aab74f35333f57422c13d79a1b
Gabe Varela <gvarela@gmail.com>
2010-11-18 11:15:55 -0700

    macvim got moved to the main repo

--- macvim/metadata.rb
+++
@@ -1,8 +0,0 @@
-maintainer       "Gabe Varela"
-maintainer_email "gvarela@gmail.com"
-license          "MIT"
-description      "Installs/configures MacVim"
-version          "0.1.0"
-
-supports "mac_os_x"
-
--- macvim/recipes/default.rb
+++
@@ -1,7 +0,0 @@
-include_recipe "homebrew"
-
-package "ctags"
-package "ack"
-
-package "macvim"
-
