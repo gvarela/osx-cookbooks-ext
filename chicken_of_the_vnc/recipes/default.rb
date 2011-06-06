2f7276a049b1cc497ce263c8db48c2f29fb1a737
Jay Zeschin <jay.zeschin@factorylabs.com>
2011-06-06 16:15:25 -0600

    Added Chicken of the VNC recipe

---
+++ chicken_of_the_vnc/recipes/default.rb
@@ -0,0 +1,22 @@
+#
+# Cookbook Name:: chicken_of_the_vnc
+# Recipe:: default
+#
+# Copyright 2011, Factory Design Labs
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+application 'Chicken of the VNC' do
+  source 'http://downloads.sourceforge.net/project/cotvnc/cotvnc/Chicken%20of%20the%20VNC%202.0b4/cotvnc-20b4.dmg'
+end
