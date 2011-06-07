0d7822cc1f5031b8632c0a213185850fc7accde7
Jay Zeschin <jay.zeschin@factorylabs.com>
2011-06-07 13:12:59 -0600

    Dropbox client recipe

---
+++ dropbox_client/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Jay Zeschin"
+maintainer_email "jay.zeschin@factorylabs.com"
+license          "Apache"
+description      "Installs Dropbox"
+version          "0.1.0"
+
+supports "mac_os_x"
