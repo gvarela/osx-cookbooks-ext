37dc3720bf6122f5515c5260a7940fd83f7316a7
Gabe Varela <gvarela@gmail.com>
2012-07-09 15:47:54 -0600

    first crack at a dotset recipe

---
+++ dotset/recipes/default.rb
@@ -0,0 +1,35 @@
+include_recipe "homebrew"
+
+package "git"
+package "ack"
+package "ctags"
+package "discount"
+package "macvim"
+package "node"
+
+npm_package "jshint"
+npm_package "jsonhint"
+
+home_dir = "/Users/#{node[:homebrew][:user]}"
+dotset_dir = "#{home_dir}/dotset"
+
+execute "git clone https://github.com/modeset/dotset.git" do
+  not_if { File.exist?("dotset")}
+  user node[:homebrew][:user]
+  cwd home_dir
+end
+
+execute "./dotset install" do
+  user node[:homebrew][:user]
+  cwd dotset_dir
+end
+
+execute "cp extras/bashrc_local.example #{home_dir}/.bashrc_local" do
+  user node[:homebrew][:user]
+  cwd dotset_dir
+end
+
+execute "cp extras/vimrc_local.example #{home_dir}/.vimrc_local" do
+  user node[:homebrew][:user]
+  cwd dotset_dir
+end
